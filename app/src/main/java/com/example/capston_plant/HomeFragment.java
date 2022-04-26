e4596a6a57a23931377b025542b5f50f17825d27
kdmstj <rkddmstj429@gmail.com>
2022-04-27 00:03:37 +0900

    complete taking data

--- app/src/main/java/com/example/capston_plant/HomeFragment.java
+++ app/src/main/java/com/example/capston_plant/HomeFragment.java
@@ -1,25 +1,30 @@
 package com.example.capston_plant;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
+import android.widget.Toast;
 
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class HomeFragment extends Fragment {
@@ -29,6 +34,12 @@ public class HomeFragment extends Fragment {
     String HUMID;
     String SOIL;
     String TANK;
+
+    TextView tv_plantName;
+    TextView tv_temp;
+    TextView tv_humid;
+    TextView tv_soil;
+    TextView tv_tank;
 
     final static private String url = "http://112.170.208.72:8920/sensorinfo.php";
 
@@ -54,23 +65,15 @@ public class HomeFragment extends Fragment {
         //fragment_home xml 파일과 연결
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
-        TextView tv_plantName = v.findViewById(R.id.tv_plantName);
-        TextView tv_temp = v.findViewById(R.id.tv_temp);
-        TextView tv_humid = v.findViewById(R.id.tv_humid);
-        TextView tv_soil = v.findViewById(R.id.tv_soil);
-        TextView tv_tank = v.findViewById(R.id.tv_tank);
+        tv_plantName = v.findViewById(R.id.tv_plantName);
+        tv_temp = v.findViewById(R.id.tv_temp);
+        tv_humid = v.findViewById(R.id.tv_humid);
+        tv_soil = v.findViewById(R.id.tv_soil);
+        tv_tank = v.findViewById(R.id.tv_tank);
 
-        System.out.println(TEMP);
-        System.out.println(HUMID);
-        System.out.println(SOIL);
-        System.out.println(TANK);
-        System.out.println(plant_name);
 
-        tv_plantName.setText(plant_name);
-        tv_temp.setText(TEMP);
-        tv_humid.setText(HUMID);
-        tv_soil.setText(SOIL);
-        tv_tank.setText(TANK);
+
+
 
         return v;
 
@@ -78,6 +81,7 @@ public class HomeFragment extends Fragment {
 
     private class GetData extends AsyncTask<String, Void, String>{
 
+        StringBuffer Buffer = new StringBuffer();
 
         @Override
         protected void onPreExecute(){
@@ -93,89 +97,85 @@ public class HomeFragment extends Fragment {
             //서버에 있는  PHP 파일을 실행시키고 응답을 저장하고 스트링으로 변환하여  리턴한다.
             //POST 방식으로 데이터 전달시에는 데이터가 주소에 직접 입력되지 않는다.
             String serverURL = params[0];
-            String get_json= "";
+            String get_json = "";
 
             //HTTP 메시지 본문에 포함되어 전송되기 때문에 따로 데이터를 준비해야 한다.
             //전송할 데이터는 “이름=값” 형식이며 여러 개를 보내야 할 경우에는 항목 사이에 &를 추가합니다.
             //여기에 적어준 이름을 나중에 PHP 에서 사용하여 값을 얻게 됩니다.
 
 
-            try{
-               // HttpURLConnection 클래스를 사용하여 POST 방식으로 데이터를 전송합니다.
+            try {
+                // HttpURLConnection 클래스를 사용하여 POST 방식으로 데이터를 전송합니다.
                 URL url = new URL(serverURL);
-                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 
-
-                //5초 안에 응답이 오지 않으면 예외처리
-                httpURLConnection.setReadTimeout(5000);
-                //5초 안에 연결이 되지 않으면 예외처리
-                httpURLConnection.setConnectTimeout(5000);
-                //요청 방식을 POST 방식으로 한다.
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoInput(true);
-                httpURLConnection.connect();
-
-                OutputStream outputStream = httpURLConnection.getOutputStream();
-                //전송할 데이터가 저장된 변수를 이곳에 입력합니다. 인코딩을 고려해줘야 합니다.
-                outputStream.flush();
-                outputStream.close();
-
-
-                int responseStatusCode = httpURLConnection.getResponseCode();
-
-
-                InputStream inputStream;
-                //응답을 읽는다.
-                if(responseStatusCode == HttpURLConnection.HTTP_OK) {
-                    //정상적인 응답
-                    inputStream = httpURLConnection.getInputStream();
+                if (conn != null) {
+                    conn.setConnectTimeout(20000);
+                    conn.setUseCaches(false);
+                    if (conn.getResponseCode() == HttpURLConnection.HTTP_OK) {
+                        // 서버에서 읽어오기 위한 스트림 객체
+                        InputStreamReader isr = new InputStreamReader(
+                                conn.getInputStream());
+                        // 줄단위로 읽어오기 위해 BufferReader로 감싼다.
+                        BufferedReader br = new BufferedReader(isr);
+                        // 반복문 돌면서읽어오기
+                        while (true) {
+                            String line = br.readLine();
+                            if (line == null) {
+                                break;
+                            }
+                            Buffer.append(line);
+                        }
+                        br.close();
+                        conn.disconnect();
+                    }
                 }
-                else{
-                    //에러발생
-                    inputStream = httpURLConnection.getErrorStream();
-                }
-
-                //StringBuilder 를 사용하여 수신되는 데이터를 저장한다.
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, "UTF-8");
-                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                get_json = Buffer.toString();
+                Log.d(TAG, "get_json: " + get_json);
 
 
-                StringBuilder sb = new StringBuilder();
-                String line;
 
-                while((line = bufferedReader.readLine()) != null){
-                    sb.append(line);
-                }
-
-                bufferedReader.close();
-
-                //저장된 데이터를 스트링으로 변환하여 리턴한다.
-                return sb.toString();
-
-            } catch (Exception e){
-
-                return null;
+            } catch (Exception e) {
+                Log.e("에러 ", e.getMessage());
             }
+            return get_json;
         }
+
 
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
+            Log.d(TAG, " <<<<<onPostExecute>>>> ");
+
+
+
 
             try {
-                JSONArray array = new JSONObject(result).getJSONArray(url);
-                if (array != null) {
-                    JSONObject jsonObject = array.getJSONObject(0);
-                    TEMP = jsonObject.getString("TEMP");
-                    HUMID = jsonObject.getString("HUMID");
-                    SOIL = jsonObject.getString("SOIL");
-                    TANK = jsonObject.getString("TANK");
-                    plant_name = jsonObject.getString("plant_name");
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray  jsonArray = jsonObject.getJSONArray("sensor");
+                JSONObject item = jsonArray.getJSONObject(0);
+                //JSONArray array = new JSONObject(result).getJSONArray(url);
+                //JSONObject jsonObject = array.getJSONObject(0);
+                if(jsonArray != null){
+                    plant_name = item.optString("plant_name","text on no value");
+                    TEMP = item.optString("TEMP","text on no value");
+                    HUMID = item.optString("HUMID","text on no value");
+                    SOIL = item.optString("SOIL","text on no value");
+                    TANK =item.optString("TANK","text on no value");
 
-                } else {
+                    tv_plantName.setText(plant_name);
+                    tv_temp.setText(TEMP);
+                    tv_humid.setText(HUMID);
+                    tv_soil.setText(SOIL);
+                    tv_tank.setText(TANK);
+                }else{
+                    System.out.println("nononono");
                 }
 
+                Log.d(TAG, TEMP + "/" + HUMID+ "/" + SOIL +"/" + TANK +"/"+plant_name);
+
             } catch (Exception e) {
+                Log.d(TAG, "showResult : ", e);
             }
         }
     }
